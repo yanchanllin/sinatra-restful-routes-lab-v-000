555b0daa86073a3837f9f04f79620b274d158d38
YanC Lin <yonalin1@gmail.com>
2018-03-25 15:49:17 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,12 +29,13 @@ class ApplicationController < Sinatra::Base
   end
 
   patch '/recipes/:id' do
+    binding.pry
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
     @recipe.save
-    binding.pry
+    
     redirect to "/recipes/#{@recipe.id}"
   end
 
